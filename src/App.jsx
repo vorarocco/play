00e70ebfdf56239cfde51cff82c381bd4e36de25
vorarocco <vora.rocco@gmail.com>
2022-03-19 11:53:53 -0400

    react-home setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Home from './home/Home';
+
+function App() {
+  return (
+    <div className="App"> 
+      <p>Hello app</p>
+      <Home/>
+
+    </div>
+  );
+}
+
+export default App;
